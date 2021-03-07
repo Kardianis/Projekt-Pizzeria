47effb36fa5b64e765bccaedbfc329e88db42edd
Kardianis <michal_kuszneruk@tlen.pl>
2021-03-07 15:18:29 +0100

    Update script.js

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -53,23 +53,71 @@
   };
 
   class Product{
-    constructor(){
+    constructor(id, data){
+      const thisProduct = this;
+        thisProduct.id = id
+        thisProduct.data = data
+      thisProduct.renderInMenu();
+      console.log('new Product', thisProduct);
+    }
+
+    renderInMenu(){
       const thisProduct = this;
 
-      console.log('new Product', thisProduct);
+      /*generate HTML based od template*/
+
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      /*create element using utils.createElementFromHTML*/
+
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /*find menu container*/
+
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /*add element to menu*/
+
+      menuContainer.appendChild(thisProduct.element);
     }
   }
 
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+      const testProdukt = newProduct;
+      console.log('testProduct', testProduct);
+    },
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
-      console.log('templates:', templates);
+      console.log('templates:', templates);      
+    
+      thisApp.initData();
+      thisApp.initMenu();
     },
+
+
   };
 
+  app.initMenu()
+    
   app.init();
+
+  app.initData();
 }
